220266b9255d8953019fef84f4ce3acb889fddae
Curt Anderson <curtanderson@gmail.com>
2017-11-20 12:24:24 +0100

    changed experiment name

--- experiments/AEvdV1/setup/settings.js
+++ experiments/AEvdV1/setup/settings.js
@@ -1,7 +1,7 @@
 var Experigen =  {
 	settings: {
 
-		experimentName: "Anderson2017ExclamativeIntonation1", // use only A-Z, a-z, 0-9
+		experimentName: "Anderson2017ExclamativeIntonation1Final", // use only A-Z, a-z, 0-9
 
 		databaseServer: "http://db.phonologist.org/",
 

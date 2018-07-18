083047f35462b16a587d18e5baeccf4ce024ebff
Curt Anderson <curtanderson@gmail.com>
2018-07-18 16:58:33 -0400

    Final version of experiment
    Uploaded final version of experiment

--- experiments/Intonation2/setup/settings.js
+++ experiments/Intonation2/setup/settings.js
@@ -1,7 +1,7 @@
 var Experigen =  {
 	settings: {
 
-		experimentName: "Anderson2017ExclamativeIntonation2Test", // use only A-Z, a-z, 0-9
+		experimentName: "Anderson2017ExclamativeIntonation2Final", // use only A-Z, a-z, 0-9
 
 		databaseServer: "http://db.phonologist.org/",
 

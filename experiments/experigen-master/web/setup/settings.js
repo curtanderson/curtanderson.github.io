038d26e220d60b4e2d980bf131acb1861791ba8c
Curt Anderson <curtanderson@gmail.com>
2018-07-20 00:22:29 -0400

    test
    testing more

--- experiments/experigen-master/web/setup/settings.js
+++ experiments/experigen-master/web/setup/settings.js
@@ -1,12 +1,12 @@
 var Experigen =  {
 	settings: {
 
-		experimentName: "Default", // use only A-Z, a-z, 0-9
-		
+		experimentName: "DefaultCurtAnderson", // use only A-Z, a-z, 0-9
+
 		databaseServer: "http://db.phonologist.org/",
 
 		online: true, // make sure you know what you're doing before changing this
-		
+
 		strings: {
 			windowTitle:     "Wug test",
 			connecting:		 "Connecting...",
@@ -16,20 +16,20 @@ var Experigen =  {
 			errorMessage:    "An error occurred. We apologize for the inconvenience.",
 			emptyBoxMessage: "Please supply your answer in the text box."
 		},
-		
+
 		audio: true,
-		
+
 		progressbar: {
-			visible: true, 
+			visible: true,
 			adjustWidth: 6,
 			percentage: false
 		},
-		
+
 		items: "resources/items.txt",
-		
+
 		otherresources: {
 			frames: "resources/frames.txt",
-			pictures: "resources/pictures.txt"	
+			pictures: "resources/pictures.txt"
 		},
 
 		folders: {
@@ -37,10 +37,8 @@ var Experigen =  {
 			sounds: "resources/sounds/",
 			pictures: "resources/pictures/"
 		},
-	
+
 		footer: "views/footer.html",
 		missingview: "views/missingview.ejs"
-	}	
+	}
 };
-
-
